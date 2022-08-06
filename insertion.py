eaa95d7189fd297d6f035691f787e851c5ede370
Shriya Dhaundiyal <shriyadh03@gmail.com>
2022-08-06 13:16:40 -0400

    insertion sort updated

--- insertion.py
+++ insertion.py
@@ -1,15 +1,41 @@
-def insertionSort(arr):
+// =================== Libraries ==================
+#include <stdio.h> // Include file for standart input/output
 
-	# Traverse through 1 to len(arr)
-	for i in range(1, len(arr)):
+// =============== Helper Functions ===============
 
-		key = arr[i]
+// Swaps two numbers in an array
+// Input: The 'address of' an index into an array for positions in an array.
+void swap(int* a, int* b){
+    // TODO: Swap two integers in an array.
+    //
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
-		# Move elements of arr[0..i-1], that are
-		# greater than key, to one position ahead
-		# of their current position
-		j = i-1
-		while j >=0 and key < arr[j] :
-				arr[j+1] = arr[j]
-				j -= 1
-		arr[j+1] = key
+
+// Name: sort
+// Input(s):
+//          (1) 'array' is a pointer to an integer address. 
+//              This is the start of some 'contiguous block of memory' that we will sort.
+//          (2) 'size' tells us how big the array of data is we are sorting.
+// Output: No value is returned, but 'array' should be modified to store a sorted array of numbers.
+void sortIntegers(int* array, unsigned int size){
+    // TODO: Implement insertion sort
+    //
+	int i;
+	int j;
+	int min;
+	for(i = 1; i < size ; i++) {
+		min = array[i];
+		j = i -1;
+		while( j >=0 && array[j] > min) {
+
+			array[j+1] =  array[j];
+			j--;
+		}
+		
+		swap(&array[j+1], &min);	
+	}
+	
+}
