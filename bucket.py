8e2892f817985a02b4a518b84671e5f9893a7e07
Shriya Dhaundiyal <shriyadh03@gmail.com>
2022-08-06 14:08:02 -0400

    editting

--- bucket.py
+++ bucket.py
@@ -1,34 +1,42 @@
-def insertionSort(b):
-    for i in range(1, len(b)):
-        up = b[i]
-        j = i - 1
-        while j >= 0 and b[j] > up:
-            b[j + 1] = b[j]
-            j -= 1
-        b[j + 1] = up
-    return b
+# Bucket Sort in Python
 
 
-def bucketSort(x):
-    arr = []
-    slot_num = 10  # 10 means 10 slots, each
-    # slot's size is 0.1
-    for i in range(slot_num):
-        arr.append([])
+def bucketSort(array):
+    bucket = []
 
-    # Put array elements in different buckets
-    for j in x:
-        index_b = int(slot_num * j)
-        arr[index_b].append(j)
+    max_element = max(array)
 
-    # Sort individual buckets
-    for i in range(slot_num):
-        arr[i] = insertionSort(arr[i])
+    min_element = min(array)
 
-    # concatenate the result
+    range_arr = int((max_element - min_element) / 10)
+
+    if range_arr < 1:
+        range_arr = 1
+
+    # Create empty buckets
+    for i in range(10):
+        bucket.append([])
+
+    # print(bucket)
+    # Insert elements into their respective buckets
+    for j in array:
+        index_b = int(j)
+
+        bucket[index_b].append(j)
+
+    # Sort the elements of each bucket
+    for i in range(len(array)):
+        bucket[i] = sorted(bucket[i])
+
+    # Get the sorted elements
     k = 0
-    for i in range(slot_num):
-        for j in range(len(arr[i])):
-            x[k] = arr[i][j]
+    for i in range(len(array)):
+        for j in range(len(bucket[i])):
+            array[k] = bucket[i][j]
             k += 1
-    return x
+    return array
+
+
+array = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+print("Sorted Array in descending order is")
+print(bucketSort(array))
